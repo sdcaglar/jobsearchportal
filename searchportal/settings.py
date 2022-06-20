2ad2019b5a6585aa9f88ad751262e29d03e99687
Seda Çağlar <seedacaglar@gmail.com>
2022-06-20 22:53:49 +0300

    changed database engine (sqlite -> postgresql)

--- searchportal/settings.py
+++ searchportal/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'searchportal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'jobportaldb',
+        'USER': 'postgres',
+        'PASSWORD': 'admin789',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
