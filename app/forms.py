3017c6cc0fe806ab2850bbcf0af440ba74226164
Seda Çağlar <seedacaglar@gmail.com>
2022-06-22 03:44:58 +0300

    added JobForm

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,6 @@
+from django.forms import ModelForm
 from .admin import UserCreationForm
-from .models import User
+from .models import User, Job
 
 
 class JobSeekerSignUpForm(UserCreationForm):
@@ -9,6 +10,7 @@ class JobSeekerSignUpForm(UserCreationForm):
     def save(self, commit=True):
         user = super().save(commit=False)
         user.is_jobseeker = True
+
         if commit:
             user.save()
         return user
@@ -24,3 +26,9 @@ class RecruiterSignUpForm(UserCreationForm):
         if commit:
             user.save()
         return user
+
+
+class JobForm(ModelForm):
+    class Meta:
+        model = Job
+        fields = ["name", "details", "company", "company_address"]
