bd28d3d7a5c07978203f9edb332156d153e4952e
Seda Çağlar <seedacaglar@gmail.com>
2022-06-22 03:48:55 +0300

    urls were added according to the created pages

--- app/urls.py
+++ app/urls.py
@@ -8,9 +8,17 @@ from .views import (
     logout_recruiter,
     HomeView,
     UserHomeView,
-    UserProfileView,
     change_password,
+    JobDetailView,
+    JobEditView,
+    JobDeleteView,
+    create_job,
+    apply_job,
+    JobEvaluationDetailView,
+    SearchView,
+    profile,
 )
+
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
@@ -21,6 +29,17 @@ urlpatterns = [
     path("recruiter/logout/", logout_recruiter, name="recruiter-logout"),
     path("recruiter/signup/", create_recruiter, name="recruiter-signup"),
     path("home", UserHomeView.as_view(), name="user-home"),
-    path("profile/<int:pk>", UserProfileView.as_view(), name="profile"),
+    path("profile", profile, name="profile"),
     path("settings/", change_password, name="settings"),
+    path("job/<int:pk>/", JobDetailView.as_view(), name="job-detail"),
+    path("job/<int:pk>/edit", JobEditView.as_view(), name="edit-job"),
+    path("job/<int:pk>/delete", JobDeleteView.as_view(), name="delete-job"),
+    path("job/create/", create_job, name="create-job"),
+    path("job/<int:pk>/apply", apply_job, name="apply-job"),
+    path(
+        "job/evaluation/<int:pk>",
+        JobEvaluationDetailView.as_view(),
+        name="evaluation-job",
+    ),
+    path("search", SearchView.as_view(), name="search-jobs"),
 ]
